ca255075e3e9bd14f6ed2c1e5e1012e2ae5e1e19
NASEELA K T <naslalellu@gmail.com>
2021-03-04 08:02:15 +0530

    changes

---
+++ day8js.js
@@ -0,0 +1,21 @@
+myFunction();
+function myFunction(){
+    var carName="volvo";
+    document.getElementById("demo1").innerHTML=carName;
+}
+myFunction();
+function varFunction(){
+    var pi=3.14;
+    var persn="Naseela";
+    var answer="Yes I am!";
+    document.getElementById("demo").innerHTML=pi+"<br>"+persn+"<br>"+answer;
+}
+varFunction();
+function addNumbers(){
+    var x=9;
+    var y=10;
+    var z=x+y;
+    document.getElementById("add").innerHTML=z;
+
+}
+addNumbers();
